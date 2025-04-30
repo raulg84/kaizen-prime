14660491894de9bdc1cf63b2a2868a4a78d051f0
[Raúl Gómez Illán] <[rgillan@uoc.edu]>
2025-04-30 15:30:51 +0200

    Recorte de imágenes de menú

--- images-optimizer.js
+++ images-optimizer.js
@@ -10,6 +10,10 @@ const outputDirImages = 'src/images/optimized'; // Ruta de salida para las imág
 const outputDirIcon = 'src/icon/optimized'; // Ruta de salida para los iconos optimizados
 
 const sizes = [480, 768, 1200]; // Tamaños de imágenes deseados
+const recortar = ['sushi-salmon.jpg', 'makis-variados.jpg', 'ramen-cerdo.jpg', 'ramen-pollo.jpg', 'te-verde.jpg', 'cheesecake-japones.jpg', 'edamame.jpg', 'gyozas.jpg'
+  , 'katsudon.jpg', 'gyudon.jpg', 'kate-udom.png', 'tempura-soba.jpg', 'yakitori.jpg', 'salmon-teriyaki.jpg', 'bento-clasico.jpg', 'bento-pollo.jpg', 'sake-tradicional.jpg'
+  , 'cerveza-asahi.jpg', 'te-verde.jpg', 'te-matcha.jpg'
+];
 
 // Creamos las carpetas de salida si no existen
 if (!fs.existsSync(outputDirImages)) {
@@ -30,21 +34,52 @@ fs.readdirSync(inputDir).forEach(file => {
   const baseName = path.parse(file).name;
 
   sizes.forEach(size => {
-    // WebP
-    sharp(inputFile)
-      .resize({ width: size })
-      .webp({ quality: 75 })
-      .toFile(path.join(outputDirImages, `${baseName}-${size}.webp`))
-      .then(() => console.log(`Creado: ${baseName}-${size}.jpg`))
-      .catch(err => console.error(`ERROR WebP ${file}:`, err));
 
-    // JPEG
-    sharp(inputFile)
-      .resize({ width: size })
-      .jpeg({ quality: 80 })
-      .toFile(path.join(outputDirImages, `${baseName}-${size}.jpg`))
-      .then(() => console.log(`Creado: ${baseName}-${size}.jpg`))
-      .catch(err => console.error(`ERROR JPEG ${file}:`, err));
+    //Imágenes de menú (Recortamos desde 0,0)
+    if (recortar.includes(path.basename(file).toLowerCase()) && 1200 != size) {
+
+      // WebP
+      sharp(inputFile)
+        .extract({ left: 0, top: 0, width: size, height: size })
+        .resize({ width: size })
+        .webp({ quality: 75 })
+        .toFile(path.join(outputDirImages, `${baseName}-${size}.webp`))
+        .then(() => console.log(`Creado: ${baseName}-${size}.jpg`))
+        .catch(err => console.error(`ERROR WebP ${file}:`, err));
+
+      // JPEG
+      sharp(inputFile)
+        .extract({ left: 0, top: 0, width: size, height: size })
+        .resize({ width: size })
+        .jpeg({ quality: 80 })
+        .toFile(path.join(outputDirImages, `${baseName}-${size}.jpg`))
+        .then(() => console.log(`Creado: ${baseName}-${size}.jpg`))
+        .catch(err => console.error(`ERROR JPEG ${file}:`, err));
+
+        //Resto de imágenes 8reducimos tamaño)
+    } else {
+
+      // WebP
+      sharp(inputFile)
+        .resize({ width: size })
+        .webp({ quality: 75 })
+        .toFile(path.join(outputDirImages, `${baseName}-${size}.webp`))
+        .then(() => console.log(`Creado: ${baseName}-${size}.jpg`))
+        .catch(err => console.error(`ERROR WebP ${file}:`, err));
+
+      // JPEG
+      sharp(inputFile)
+        .resize({ width: size })
+        .jpeg({ quality: 80 })
+        .toFile(path.join(outputDirImages, `${baseName}-${size}.jpg`))
+        .then(() => console.log(`Creado: ${baseName}-${size}.jpg`))
+        .catch(err => console.error(`ERROR JPEG ${file}:`, err));
+
+    }
+
+
+
+
   });
 });
 
